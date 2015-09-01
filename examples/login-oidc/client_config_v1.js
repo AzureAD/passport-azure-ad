976d7c8a77f30385f51e20eda9f3f9662b8cb855
Brandon Werner <brandwe@microsoft.com>
2015-09-01 12:30:28 -0700

    minor fix

--- examples/login-oidc/client_config_v1.js
+++ examples/login-oidc/client_config_v1.js
@@ -3,10 +3,10 @@
  exports.creds = {
  	returnURL: 'http://localhost:3000/auth/openid/return',
  	identityMetadata: 'https://login.microsoftonline.com/common/.well-known/openid-configuration', // For using Microsoft you should never need to change this.
- 	clientID: '<your app id>',
- 	clientSecret: '<your app secret>', // if you are doing code or id_token code
+ 	clientID: 'c9655d1d-f356-46a7-afe1-431c0d6eeb37',
+ 	clientSecret: 'ojEsyBUEv0huRz7gKRqbHmv9WbrGXKFtQ0w+/UNQT1E=', // if you are doing code or id_token code
  	skipUserProfile: true, // for AzureAD should be set to true.
  	responseType: 'id_token code', // for login only flows use id_token. For accessing resources use `id_token code`
- 	responseMode: 'form_post', // For login only flows we should have token passed back to us in a POST
+ 	responseMode: 'query', // For login only flows we should have token passed back to us in a POST
  	//scope: ['email', 'profile'] // additional scopes you may wish to pass
  	};
