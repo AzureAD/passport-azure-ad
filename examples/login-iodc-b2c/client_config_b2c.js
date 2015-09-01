24c97111874e5e1baca5fa219265033b8530b443
Brandon Werner <brandwe@microsoft.com>
2015-08-31 17:41:57 -0700

    B2c Support - also refactored configuration away from main thread and in to waterfalls[];

---
+++ examples/login-iodc-b2c/client_config_b2c.js
@@ -0,0 +1,15 @@
+  // Don't commit this file to your public repos. This config is for first-run
+  //
+ exports.creds = {
+ 	returnURL: 'http://localhost:3000/auth/openid/return',
+ 	identityMetadata: 'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration', // For using Microsoft you should never need to change this.
+ 	clientID: 'd5eccd65-1b82-4c2a-8bc3-35b31ca408cd',
+ 	clientSecret: 'dWhryEehDmXi3A6A0fNebKo', // if you are doing code or id_token code
+ 	skipUserProfile: true, // for AzureAD should be set to true.
+ 	responseType: 'id_token', // for login only flows use id_token. For accessing resources use `id_token code`
+ 	responseMode: 'form_post', // For login only flows we should have token passed back to us in a POST
+ 	//scope: ['email', 'profile'] // additional scopes you may wish to pass
+ 	
+ 	// B2C Config
+ 	tenantName: 'hypercubeb2c.onmicrosoft.com'
+ 	};
