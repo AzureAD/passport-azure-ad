8768d3bb7c3e93bd50ac93562d22a098d5a09dc6
Brandon Werner <brandwe@microsoft.com>
2015-09-03 12:02:15 -0700

    Updated all samples to validate issuer

--- examples/server-oauth2-b2c/server_config.js
+++ examples/server-oauth2-b2c/server_config.js
@@ -4,7 +4,7 @@
      clientID: '8fcf9b97-0489-4ead-b1eb-23254690dddc',
      clientSecret: 'LlFBJFY3Y2J9YnhBLW1UKDZNRSg=',
      audience: 'http://kidventus.net/TodoListService',
-     identityMetadata: 'https://login.microsoftonline.com/common/.well-known/openid-configuration', // For using Microsoft you should never need to change this.
+     identityMetadata: 'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration', // For using Microsoft you should never need to change this.
      tenantName:'hypercubeb2c.onmicrosoft.com',
      policyName:'B2C_1_b2c_node_signin',
      validateIssuer: true
