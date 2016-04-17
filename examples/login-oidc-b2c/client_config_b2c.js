562fb5766a99fd8deeb1c0f21c75abd43cd57e43
Brandon Werner <brandwe@microsoft.com>
2016-04-17 12:48:13 -0700

    Fixed email error. Need to backport to samples now.

--- examples/login-oidc-b2c/client_config_b2c.js
+++ examples/login-oidc-b2c/client_config_b2c.js
@@ -4,6 +4,7 @@
  	returnURL: 'http://localhost:3000/auth/openid/return',
  	identityMetadata: 'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration', // For using Microsoft you should never need to change this.
  	clientID: '316f8e7c-8d01-49ed-87cd-5da26738dd18',
+	clientSecret: 'Wr6CY6ho]7|st&/n', // if you are doing code or id_token code
  	skipUserProfile: true, // for AzureAD should be set to true.
  	responseType: 'id_token', // for login only flows use id_token. For accessing resources use `id_token code`
  	responseMode: 'form_post', // For login only flows we should have token passed back to us in a POST
